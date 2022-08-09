f5ce8665f1bc24ca3f3870982657ce916d98a138
Aaron Rasing <maaronrasing@gmail.com>
2022-08-09 23:38:02 +0800

    Added a converter

--- bus-hub-management/src/main/java/com/example/bushub/management/config/OAuth2ResourceServerSecurityConfig.java
+++ bus-hub-management/src/main/java/com/example/bushub/management/config/OAuth2ResourceServerSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.bushub.management.config;
 
+import com.example.bushub.management.config.jwt.CustomJwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class OAuth2ResourceServerSecurityConfig {
+
+  private final CustomJwtAuthConverter customJwtAuthConverter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -15,9 +19,17 @@ public class OAuth2ResourceServerSecurityConfig {
       .anyRequest()
       .hasAuthority("SCOPE_mgmt")
       .and()
-      .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+      .oauth2ResourceServer(oauth2 -> oauth2
+        .jwt(jwt -> jwt.jwtAuthenticationConverter(customJwtAuthConverter))
+      );
 
     return httpSecurity.build();
   }
+
+//  @Bean
+//  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        grantedAuthoritiesConverter.setAuthoritiesClaimName();
+//  }
 
 }
---
+++ bus-hub-management/src/main/java/com/example/bushub/management/config/jwt/CustomJwtAuthConverter.java
@@ -0,0 +1,28 @@
+package com.example.bushub.management.config.jwt;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class CustomJwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+  private final Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+  private static final String principalClaimName = "username";
+
+  @Override
+  public final AbstractAuthenticationToken convert(Jwt jwt) {
+    Collection<GrantedAuthority> authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+    final String principalClaimValue = jwt.getClaimAsString(principalClaimName);
+
+    // Authentication object can be customized through this.
+    return new JwtAuthenticationToken(jwt, authorities, principalClaimValue);
+  }
+}
